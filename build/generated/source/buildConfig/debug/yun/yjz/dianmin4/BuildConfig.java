c33f7cdac0ebc7e681992bff0056818d356b43bb
jiafazhan <2811972101@qq.com>
2017-05-19 15:39:24 +0800

    蓝牙点名

---
+++ build/generated/source/buildConfig/debug/yun/yjz/dianmin4/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package yun.yjz.dianmin4;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "yun.yjz.dianmin4";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1";
+}
